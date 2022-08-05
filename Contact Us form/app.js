022b59b746a6602f2ae791796fd15fb80ef4e6f5
aarshkhare26 <aarshkhare26@gmail.com>
2022-08-05 14:15:12 +0530

    .

---
+++ Contact Us form/app.js
@@ -0,0 +1,21 @@
+// Removing Page Refresh 
+const submit = document.querySelector('#btn-submit'),
+form = document.querySelector('#form');
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  form.reset();
+}, false);
+
+// For Side Nav Bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
